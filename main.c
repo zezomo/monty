8d5604cd720a9bb93810cbe96a89897c9e745351
zezomo <zezouniverse7@gmail.com>
2024-02-07 12:45:50 -0800

    adwga

--- main.c
+++ main.c
@@ -1,35 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
-} stack_t;
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+#include "monty.h"
 
 /**
  * push - Pushes an element onto the stack.
@@ -38,28 +7,26 @@ typedef struct instruction_s
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-    /* Check if there is an argument given to push */
-    if (!stack || !*stack)
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	stack_t *new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = atoi((*stack)->opcode);
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    new_node->n = atoi((*stack)->opcode);
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	if (*stack)
+		(*stack)->prev = new_node;
 
-    if (*stack)
-        (*stack)->prev = new_node;
-
-    *stack = new_node;
+	*stack = new_node;
 }
 
 /**
@@ -69,33 +36,28 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    (void)line_number; /* Unused parameter */
-
-    stack_t *current = *stack;
-
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	(void)line_number;
+	
+	stack_t *current = *stack;
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
 
 int main()
 {
-    stack_t *stack = NULL;  /* Initialize an empty stack */
-    instruction_t instructions[] = {
-        {"push", push},
-        {"pall", pall},
-        /* Add other opcodes and their corresponding functions here */
-        {NULL, NULL} /* Terminator for the instruction array */
-    };
-
-    /* Example of using the instructions */
-    instructions[0].f(&stack, 1); /* push 1 */
-    instructions[0].f(&stack, 2); /* push 2 */
-    instructions[0].f(&stack, 3); /* push 3 */
-    instructions[1].f(&stack, 4); /* pall */
-
-    return (0);
+	stack_t *stack = NULL;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+	instructions[0].f(&stack, 1);
+	instructions[0].f(&stack, 2);
+	instructions[0].f(&stack, 3);
+	instructions[1].f(&stack, 4);
+	return (0);
 }
 
